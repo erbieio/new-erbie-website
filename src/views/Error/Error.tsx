93d79a7f6ab7e6fadec26e93fa1dac8a1e235e4e
Jason Chen <447462131@qq.com>
2024-05-29 17:50:41 +0800

    feat: add page error

--- src/views/Error/Error.tsx
+++ src/views/Error/Error.tsx
@@ -1,6 +1,25 @@
-
+import "./Error.scss";
+import { useNavigate } from "react-router-dom";
 export default function Error() {
+  const navigator = useNavigate();
+  const handleClick = () => {
+    navigator("/", { replace: true });
+  };
   return (
-    <div>Error</div>
-  )
+    <div className="w-100vw h-100vh flex items-center justify-center page-error">
+      <div>
+        <div className="font-size-150px font-bold h-130px flex justify-center">
+          <i className="i-tabler-error-404 color-#dedcdc"></i>
+        </div>
+        <div className="font-size-14px text-center color-gray">
+          Page Not Found
+        </div>
+        <div className="mt-20px">
+          <div className="lh-40px back-home-btn" onClick={handleClick}>
+            Back to the home page
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
