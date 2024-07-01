c21727854763ca443afda67f0d9bb010536c3784
Jason Chen <447462131@qq.com>
2024-07-01 15:59:50 +0800

    feat: add agreements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Footer() {
+  const navigator = useNavigate();
   return (
     <div className="flex footer px-16px lg:px-40px py-24px lg:h-9vh lg:py-0 font-size-12px font-bold items-center  border-t-width-1px border-t-style-solid border-t-color-#2A1F32">
       <div className="flex-col lg:flex-row flex justify-between flex-1">
         <div className="text-left">Copyright © 2024 - Erbie</div>
         <div className="flex mt-16px lg:mt-0">
           <div>
-            <a href="https://demo.erbie.io/PrivacyNotice" className="link color-#777 " target="__blank">
+            <span
+              className="link color-#777 "
+              onClick={() => navigator("/PrivacyPolicy")}
+            >
               Privacy Policy
-            </a>
+            </span>
           </div>
           <div className="ml-24px lg:ml-40px">
-            <a href="https://demo.erbie.io/TermsOfService" className="link color-#777 " target="__blank">
+            <span
+              className="link color-#777 "
+              onClick={() => navigator("/TermsOfUse")}
+            >
               Terms of Use
-            </a>
+            </span>
           </div>
         </div>
       </div>
--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,95 +1,114 @@
 /* eslint-disable react-refresh/only-export-components */
 
-
 import { createBrowserRouter } from "react-router-dom";
 
-import App from  '../App.tsx';
+import App from "../App.tsx";
 import LazyLoad from "./LazyLoad.tsx";
 import { lazy } from "react";
 import Home from "../views/Home/Home.tsx";
 import Docs from "../views/Docs/Docs.tsx";
 import Explorer from "../views/Explorer/Explorer.tsx";
-import ExplorerHome from '../views/Explorer/Pages/Home/Home.tsx'
-import Infrastructure from '../views/Infrastructure/Infrastructure.tsx'
+import ExplorerHome from "../views/Explorer/Pages/Home/Home.tsx";
+import Infrastructure from "../views/Infrastructure/Infrastructure.tsx";
 import Wallet from "../views/Wallet/Wallet.tsx";
+
 const Error = lazy(() => import("../views/Error/Error.tsx"));
 // const Wallet = lazy(() => import("../views/Wallet/Wallet.tsx"));
-const Chain = lazy(() => import('../views/Explorer/Pages/Chain/Chain.tsx'))
-const Builder = lazy(() => import('../views/Explorer/Pages/Builder/Builder.tsx'))
-const Csbt = lazy(() => import('../views/Explorer/Pages/Csbt/Csbt.tsx'))
-const Asset = lazy(() => import('../views/Explorer/Pages/Asset/Asset.tsx'))
-const Account = lazy(() => import('../views/Explorer/Pages/Account/Account.tsx'))
-const Transct = lazy(() => import('../views/Explorer/Pages/Transct/Transct.tsx'))
-const AccountDetail = lazy(() => import('../views/Explorer/Pages/AccountDetail/AccountDetail.tsx'))
-const BlockDetail = lazy(() => import('../views/Explorer/Pages/BlockDetail/BlockDetail.tsx'))
+const Chain = lazy(() => import("../views/Explorer/Pages/Chain/Chain.tsx"));
+const Builder = lazy(
+  () => import("../views/Explorer/Pages/Builder/Builder.tsx")
+);
+const Csbt = lazy(() => import("../views/Explorer/Pages/Csbt/Csbt.tsx"));
+const Asset = lazy(() => import("../views/Explorer/Pages/Asset/Asset.tsx"));
+const Account = lazy(
+  () => import("../views/Explorer/Pages/Account/Account.tsx")
+);
+const Transct = lazy(
+  () => import("../views/Explorer/Pages/Transct/Transct.tsx")
+);
+const AccountDetail = lazy(
+  () => import("../views/Explorer/Pages/AccountDetail/AccountDetail.tsx")
+);
+const BlockDetail = lazy(
+  () => import("../views/Explorer/Pages/BlockDetail/BlockDetail.tsx")
+);
+const PrivacyPolicy = lazy(() => import("../views/Agreement/PrivacyPolicy"));
+const TermsOfUse = lazy(() => import("../views/Agreement/TermsOfUse"));
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-      errorElement: LazyLoad(Error),
-      children: [
-        {
-            path: '/',
-            element: <Home />
-        },
-        {
-          path: '/infrastructure',
-          element: <Infrastructure/>
-        },
-        {
-            path: '/docs',
-            element: <Docs />
-        },
-        {
-            path: '/explorer',
-            element: <Explorer />,
-            children: [
-              {
-                path: '/explorer/home',
-                element: <ExplorerHome />,
-              },
-              {
-                path: '/explorer/chain',
-                element: LazyLoad(Chain),
-              },
-              {
-                path: '/explorer/builder',
-                element: LazyLoad(Builder),
-              },
-              {
-                path: '/explorer/csbt',
-                element: LazyLoad(Csbt),
-              },
-              {
-                path: '/explorer/asset',
-                element: LazyLoad(Asset),
-              },
-              {
-                path: '/explorer/account',
-                element: LazyLoad(Account),
-              },
-              {
-                path: '/explorer/transct',
-                element: LazyLoad(Transct),
-              },
-              {
-                path: '/explorer/accountDetail/:address',
-                element: LazyLoad(AccountDetail),
-              },
-              {
-                path: '/explorer/blockDetail/:blockNumber',
-                element: LazyLoad(BlockDetail),
-              },
-            ]
-        },
-        {
-            path: '/wallet',
-            element: <Wallet />
-        },
-      ]
-    },
-
-  ]);
+  {
+    path: "/",
+    element: <App />,
+    errorElement: LazyLoad(Error),
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/infrastructure",
+        element: <Infrastructure />,
+      },
+      {
+        path: "/docs",
+        element: <Docs />,
+      },
+      {
+        path: "/explorer",
+        element: <Explorer />,
+        children: [
+          {
+            path: "/explorer/home",
+            element: <ExplorerHome />,
+          },
+          {
+            path: "/explorer/chain",
+            element: LazyLoad(Chain),
+          },
+          {
+            path: "/explorer/builder",
+            element: LazyLoad(Builder),
+          },
+          {
+            path: "/explorer/csbt",
+            element: LazyLoad(Csbt),
+          },
+          {
+            path: "/explorer/asset",
+            element: LazyLoad(Asset),
+          },
+          {
+            path: "/explorer/account",
+            element: LazyLoad(Account),
+          },
+          {
+            path: "/explorer/transct",
+            element: LazyLoad(Transct),
+          },
+          {
+            path: "/explorer/accountDetail/:address",
+            element: LazyLoad(AccountDetail),
+          },
+          {
+            path: "/explorer/blockDetail/:blockNumber",
+            element: LazyLoad(BlockDetail),
+          },
+        ],
+      },
+      {
+        path: "/wallet",
+        element: <Wallet />,
+      },
+      {
+        path: "/PrivacyPolicy",
+        element: LazyLoad(PrivacyPolicy),
+      },
+      {
+        path: "/TermsOfUse",
+        element: LazyLoad(TermsOfUse),
+      },
+    ],
+  },
+]);
 
 export default router;
