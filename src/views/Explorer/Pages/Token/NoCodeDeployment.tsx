1db4bc8361eee98ff09d2c5a10ed533489973fab
Jason Chen <447462131@qq.com>
2024-11-01 15:52:12 +0800

    feat: page deployment support mobile

--- src/views/Explorer/Pages/Token/NoCodeDeployment.tsx
+++ src/views/Explorer/Pages/Token/NoCodeDeployment.tsx
@@ -40,7 +40,7 @@ const NoCodeDeployment = () => {
     messageApi.warning("Coming soon");
   };
   return (
-    <div className="no-code-deployment">
+    <div className="no-code-deployment pb-20px lg:pb-0">
       {messageContext}
       <div className="font-size-12px text-left">
         This method is suitable for users who lack programming sks. There is no
@@ -64,7 +64,7 @@ const NoCodeDeployment = () => {
         </div>
       </div>
       <div className="flex flex-col justify-start deployment-form">
-        <div className="flex justify-between gap-14px">
+        <div className="flex flex-col lg:flex-row justify-between gap-14px">
           <div className="flex-1">
             <div className="text-left label">TOKEN Name</div>
             <div>
@@ -90,14 +90,14 @@ const NoCodeDeployment = () => {
             </div>
           </div>
         </div>
-        <div className="flex gap-14px mt-2vh">
-          <div className="w-24.3%">
+        <div className="flex flex-col lg:flex-row gap-14px mt-2vh">
+          <div className="w-100% lg:w-24.3%">
             <div className="text-left label">Totalsupply</div>
             <div>
               <input placeholder="Enter" type="text" />
             </div>
           </div>
-          <div className="w-75.7% flex justify-start font-size-12px text-left mt-3vh">
+          <div className="w-100% lg:w-75.7% flex justify-start font-size-12px text-left mt-0px lg:mt-3vh">
             The Total Supply is the actual total supply * 10^n, where n is the
             number of decimals.The Total Supply is the actual total supply *
             10^n, where n is the number of decimals.
